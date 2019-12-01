5783f495c67e27f8649d28544acbd4956b4c9641
vincent290587 <vincent.golle@gmail.com>
2019-12-01 11:45:30 +0100

    New board

--- custom_board_v1.h
+++ custom_board_v1.h
@@ -19,7 +19,7 @@ extern "C" {
 #define LEDS_NUMBER    1
 
 //#define LED_START      14
-#define LED_1            NRF_GPIO_PIN_MAP(0, 26)
+#define LED_1            NRF_GPIO_PIN_MAP(0, 17)
 //#define LED_STOP       14
 
 #define LEDS_ACTIVE_STATE 1
@@ -36,7 +36,7 @@ extern "C" {
 #define BUTTONS_NUMBER 1
 
 //#define BUTTON_START   17
-#define BUTTON_1       NRF_GPIO_PIN_MAP(0, 25)
+#define BUTTON_1       NRF_GPIO_PIN_MAP(0, 13)
 //#define BUTTON_STOP    19
 #define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
 
@@ -46,19 +46,19 @@ extern "C" {
 
 #define BSP_BUTTON_0   BUTTON_1
 
-#define SDA_PIN_NUMBER   NRF_GPIO_PIN_MAP(0, 11)
-#define SCL_PIN_NUMBER   NRF_GPIO_PIN_MAP(0, 12)
+#define SDA_PIN_NUMBER   NRF_GPIO_PIN_MAP(0, 25)
+#define SCL_PIN_NUMBER   NRF_GPIO_PIN_MAP(0, 24)
 
-#define LSM6_INT1        NRF_GPIO_PIN_MAP(0, 13)
-#define LSM6_INT2        NRF_GPIO_PIN_MAP(0, 14)
+#define LSM6_INT1        NRF_GPIO_PIN_MAP(0, 23)
+#define LSM6_INT2        NRF_GPIO_PIN_MAP(0, 26)
 
-#define VL53_INT         NRF_GPIO_PIN_MAP(0, 15)
+#define VL53_INT         NRF_GPIO_PIN_MAP(0, 22)
 
-#define VNH_INA1         NRF_GPIO_PIN_MAP(0, 22)
-#define VNH_INB1         NRF_GPIO_PIN_MAP(0, 23)
-#define VNH_PWM1         NRF_GPIO_PIN_MAP(0, 24)
-#define VNH_CS1          NRF_GPIO_PIN_MAP(0, 25)
-#define VNH_DIAG1        NRF_GPIO_PIN_MAP(0, 25)
+#define VNH_INA1         NRF_GPIO_PIN_MAP(0, 4)
+#define VNH_INB1         NRF_GPIO_PIN_MAP(0, 28)
+#define VNH_PWM1         NRF_GPIO_PIN_MAP(0, 29)
+#define VNH_CS1          NRF_GPIO_PIN_MAP(0, 30)
+#define VNH_DIAG1        NRF_GPIO_PIN_MAP(0, 31)
 
 #ifdef __cplusplus
 }
