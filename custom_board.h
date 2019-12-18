38678917bacb2b59e67ef5bb923def6bd372514c
vincent290587 <vincent.golle@gmail.com>
2019-12-18 09:21:55 +0100

    Added pca10040 board for debugging

--- custom_board.h
+++ custom_board.h
@@ -2,6 +2,14 @@
 #ifndef BOARD_CUSTOM_H
 #define BOARD_CUSTOM_H
 
+#if defined( CUSTOM_V0 )
+
+#include "custom_board_v0.h"
+
+#else
+
 #include "custom_board_v1.h"
+
+#endif
 
 #endif // BOARD_CUSTOM_H
