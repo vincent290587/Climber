5c7da26a91c202764d1ce09b906b03c45aac02dd
v.golle <v.golle@isispace.nl>
2019-12-19 15:59:10 +0100

    Multiple offsets

--- rf/ble_api6.c
+++ rf/ble_api6.c
@@ -600,11 +600,17 @@ void ble_nus_tasks(void) {
 
 		// calibration commands
 		case 'U':
-			data_dispatcher__offset_calibration(  5 );
-			break;
+		{
+			for (int i=0; i < m_nus_xfer_rx_array.length; i++) {
+				if (m_nus_xfer_rx_array.p_xfer_str[0] == 'U') data_dispatcher__offset_calibration(  5 );
+			}
+		} break;
 		case 'D':
-			data_dispatcher__offset_calibration( -5 );
-			break;
+		{
+			for (int i=0; i < m_nus_xfer_rx_array.length; i++) {
+				if (m_nus_xfer_rx_array.p_xfer_str[0] == 'D') data_dispatcher__offset_calibration( -5 );
+			}
+		} break;
 
 		default:
 			LOG_WARNING("Unknown NUS packet");
